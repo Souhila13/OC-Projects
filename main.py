51d74aa99d15ed4eb555e2c7c7f75161e4dadb35
Souhila13 <souhila.bourmada@gmail.com>
2021-06-22 08:45:43 +0200

    Ajout du dictionnaire

--- main.py
+++ main.py
@@ -1,15 +1,76 @@
 import requests
 from bs4 import BeautifulSoup 
 
-url = "http://books.toscrape.com/"
+url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 
 response = requests.get(url)
 
 if response.ok:
     soup = BeautifulSoup(response.text, 'html.parser')
-     header = soup.find('header')
-    print('header')
+    header = soup.find_all('div', {'class': 'product_main'})
+    print(header[0])
+
+    titre = header[0].find('h1').text
+    print(titre)
+
+    page = soup.find_all('article', {'class': 'product_page'})
+    print(page[0].text)
+
+    book = {}
+    book["name"] = "A Light in the Attic"
+    book["price"] = "51.77"
+
+    """for valeur in book.values(): 
+        print(valeur)"""
+
+    for cle, valeur in book.items(): 
+        print (cle, valeur)   
+    
+    
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+    
    
+    
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+        
+
+        
+
+
+
+
+
+
+
+
+
+
+
   
 
 
