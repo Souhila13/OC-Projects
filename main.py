3ca02eabb0d9410df34aed015d8e5a5012d7c12c
Souhila13 <souhila.bourmada@gmail.com>
2021-08-04 12:56:52 +0200

    à commenter

--- main.py
+++ main.py
@@ -1,58 +1,123 @@
 import requests
 from bs4 import BeautifulSoup 
 
+
+def livre(url):
+    
+    response = requests.get(url)
+
+    if response.ok:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        header = soup.find_all('div', {'class': 'product_main'})
+        print(header[0])
+
+        titre = header[0].find('h1').text
+        print(titre)
+
+        page = soup.find_all('article', {'class': 'product_page'})
+        print(page[0].text)
+
+        table = soup.table.find_all('td')
+        
+        book = {}
+        
+        code_barre = table[0].text
+        prix = table[3].text
+        inclu = table[3].text
+        exclu = table[2].text
+        stock = table[5].text
+        vue = table[6].text
+
+        image_url = "http://books.toscrape.com" + soup.img['src'][5:]
+        
+        book["image_url"] = image_url
+        print(image_url)
+
+        book["name"] = titre
+        book["price"] = prix
+        book["code"] = code_barre 
+
+        book["price_inclu"]= inclu
+        book["price_exclu"] = exclu
+        book["available"] = stock
+        book["review_rating"] = vue
+        
+        return book
+    """description = soup.find_all('p')[3].text """
+    
 url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+    
+print(livre(url))
+
+
+
+url = "https://books.toscrape.com/"
+
+response = requests.get(url)
+
+if response.ok: 
+    link_r = []
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = soup.find_all('div', {'class': 'image_container'})
+    for div in links: 
+        a = div.find('a')
+        link = a['href']
+        link_r.append("https://books.toscrape.com/" + link)
+    
+print(link_r)
+
+for book in link_r: 
+    print(livre(book))
+
+url = "https://books.toscrape.com/catalogue/category/books_1/index.html" 
 
 response = requests.get(url)
 
 if response.ok:
+    category = []
     soup = BeautifulSoup(response.text, 'html.parser')
-    header = soup.find_all('div', {'class': 'product_main'})
-    print(header[0])
-
-    titre = header[0].find('h1').text
-    print(titre)
-
-    page = soup.find_all('article', {'class': 'product_page'})
-    print(page[0].text)
-
-    table = soup.table.find_all('td')
+    links = soup.find_all('div', {'class': 'page_inner'})
     
-    book = {}
+    for category in url: 
+        a = div.find('li', {'class': 'active'})
+        url.replace("https://books.toscrape.com/catalogue/category/books_1/index.html" + link)
+
+print(category)
+
+
+
+
+
+
+        
+
+        
     
-    code = table[0].text
-    prix = table[3].text
-    inclu = table[3].text
-    exclu = table[2].text
-    stock = table[5].text
-    vue = table[6].text
     
-    image = soup.find('div', {'id': 'product_gallery'}).text
-    book["image_url"] = image
-    print(image)
 
-    book["name"] = titre
-    book["price"] = prix
-    book["code"] = code 
 
-    book["price_inclu"]= inclu
-    book["price_exclu"] = exclu
-    book["available"] = stock
-    book["review_rating"] = vue
+
+
+
+
+
     
-     
 
+ 
 
-    """description = soup.find_all('p')[3].text """
     
-    print(book)
+        
+
+   
 
 
 
-    """for valeur in book.values(): 
-        print(valeur) """ 
-    """for cle in book.keys(): 
-        print (cle) """
+
+
+
+
+
+
 
     
     
