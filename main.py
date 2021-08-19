02a64cbcf242399eccfcae80c3638addfc300ce2
Souhila13 <souhila.bourmada@gmail.com>
2021-08-19 11:50:03 +0200

    csv

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 from pprint import pprint
+import csv
 
 def livre(url):
     """ Fonction qui réccupère les informations d'un livre
      et les place dans un dictionnaire"""
-   
+
     response = requests.get(url)
     if response.ok:
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -17,47 +18,66 @@ def livre(url):
         book["name"] = titre
         book["image_url"] = "http://books.toscrape.com" + soup.img['src'][5:]
         book["price"] = table[3].text
-        book["code"] = table[0].text 
+        book["code"] = table[0].text
         book["price_inclu"]= table[3].text
         book["price_exclu"] = table[2].text
         book["available"] = table[5].text
         book["review_rating"] = table[6].text
-        """book["descrition"] = soup.find_all('p')[3].text"""
-
+        book["descrition"] = soup.find_all('p')[3].text
         return book
 
-    # test sur une url
-print(livre("http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"))
-        
-url = "https://books.toscrape.com/"        
-   
-response = requests.get(url)
-if response.ok:
-    links_r = []
-    soup = BeautifulSoup(response.text, 'html.parser')
-    links = soup.find_all('div', {'class': 'image_container'})
-    for div in links:
-        a = div.find('a')
-        link = a['href']
-        links_r.append("https://books.toscrape.com/" + link)
+# test sur une url
+#pprint(livre(http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html))
 
-print(links_r)
+def clear(titre):
+    """une petite fonction pour nettoyer les titres, enlever les espaces en trop"""
+    propre = ""
+    for lettre in titre:
+        if lettre != " ":
+            propre += lettre
+    return propre
 
-url = "https://books.toscrape.com/catalogue/category/books/travel_2/index.html"
+def liste_categories():
+    """Retourne un dictionnaire avec toutes les catégories"""
+    url = "http://books.toscrape.com/catalogue/category/books_1/index.html"
+    page = requests.get(url)
+    html = page.text
+    soup = BeautifulSoup(html, "lxml")
+    liens = soup.aside.find_all('a')
+    categories = {}
+    for lien in liens[1:]:
+        url_cat = "http://books.toscrape.com/catalogue/category/" + lien['href'][3:]
+        categories[clear(lien.text.replace("\n", ""))] = url_cat
+    return categories
 
- 
-if response.ok:
-    category = []
-    soup = BeautifulSoup(response.text, 'html.parser')
-    links =soup.find_all('div', {'class': 'side_categories'})
-    for li in links:
-        categorie = li.find('ul')
-        link = a['href']
-        
-        print(categorie)
-        #category.append("https://books.toscrape.com/catalogue/category/books/travel_2/index.html" + link)   
-"""
-for book in links_r:
-    print(livre(book))
-"""
-  
+pprint(liste_categories())
+
+# Maintenant que nous avons la liste des catégories et leur url, il faut réccupérer les livres de chaque catégorie
+
+url = "https://books.toscrape.com/"
+
+def get_all(url):
+    """ cette fonction pas utilisée permet de réccupérer tous les livres
+    attention, pour le moment elle ne prend que la première page"""
+    response = requests.get(url)
+    if response.ok:
+        links_r = []
+        soup = BeautifulSoup(response.text, 'html.parser')
+        links = soup.find_all('div', {'class': 'image_container'})
+        for div in links:
+            a = div.find('a')
+            link = a['href']
+            links_r.append("https://books.toscrape.com/" + link)
+
+    print(links_r)
+
+    for book in links_r:
+        print(livre(book))
+
+
+with open('librairie.csv', 'r') as file:
+    reader = csv.reader(file)
+    for link in reader: 
+        file.reader(link + '\n')
+
+
